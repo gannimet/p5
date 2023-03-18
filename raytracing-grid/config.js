79eaff531cccccc93812493133f91b5c77c8f8a0
Richard Wotzlaw <r.wotzlaw@gmail.com>
2023-03-18 19:53:06 +0100

    add some stripes to walls to make them look and feel more organic

--- raytracing-grid/config.js
+++ raytracing-grid/config.js
@@ -1,8 +1,8 @@
 const config = {
   showGridWindow: true,
-  gridWindowSize: 200,
+  gridWindowSize: 300,
   playerSpeed: 0.03,
-  turnAngle: 0.05,
+  turnAngle: 0.03,
   fieldOfViewDegrees: 70,
   rayIntervalDegrees: 0.1,
   outputCellCoords: false,
